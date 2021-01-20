f6210ab475d3f039cd91784930bf2e45ae1ae837
yuanxuedao <yuanxuedao@zailingtech.com>
2021-01-20 13:59:30 +0800

    修改部分显示字段

--- src/views/BoardConfigManage/BoardConfigManage.vue
+++ src/views/BoardConfigManage/BoardConfigManage.vue
@@ -1,42 +1,79 @@
 <template>
-  <div class="manage-board__container" v-loading="loading">
-    <el-tabs v-model="activeName" @tab-click="handleClick">
-      <el-tab-pane :label="'全部大屏('+statForm.stasCount+')'" name="0"></el-tab-pane>
-      <el-tab-pane :label="'使用中('+statForm.inUse+')'" :name="IN_USE"></el-tab-pane>
-      <el-tab-pane :label="'待审核('+statForm.toBeReviewed+')'" :name="CHECK_PENDING"></el-tab-pane>
-      <el-tab-pane :label="'待发布('+statForm.toBeReleased+')'" :name="PUBLISH_PENDING"></el-tab-pane>
-      <el-tab-pane :label="'退回('+statForm.returnScr+')'" :name="RETURN_BACK"></el-tab-pane>
-      <el-tab-pane :label="'已删除('+statForm.deleted+')'" :name="DELETED"></el-tab-pane>
+  <div class="manage-board__container"
+       v-loading="loading">
+    <el-tabs v-model="activeName"
+             @tab-click="handleClick">
+      <el-tab-pane :label="'全部大屏('+statForm.stasCount+')'"
+                   name="0"></el-tab-pane>
+      <el-tab-pane :label="'使用中('+statForm.inUse+')'"
+                   :name="IN_USE"></el-tab-pane>
+      <el-tab-pane :label="'待审核('+statForm.toBeReviewed+')'"
+                   :name="CHECK_PENDING"></el-tab-pane>
+      <el-tab-pane :label="'待提交('+statForm.toBeReleased+')'"
+                   :name="PUBLISH_PENDING"></el-tab-pane>
+      <el-tab-pane :label="'驳回('+statForm.returnScr+')'"
+                   :name="RETURN_BACK"></el-tab-pane>
+      <el-tab-pane :label="'已删除('+statForm.deleted+')'"
+                   :name="DELETED"></el-tab-pane>
     </el-tabs>
     <div class="search__container">
       <div class="search_form">
-        <el-form :model="form" label-width="90px" inline>
+        <el-form :model="form"
+                 label-width="90px"
+                 inline>
           <el-form-item label="大屏名称：">
-            <el-input clearable @change="handleGetDataList" :style="{width: '200px'}" placeholder="请输入大屏名称" v-model="form.screenName"></el-input>
+            <el-input clearable
+                      @change="handleGetDataList"
+                      :style="{width: '200px'}"
+                      placeholder="请输入大屏名称"
+                      v-model="form.screenName"></el-input>
           </el-form-item>
           <el-form-item label="创建日期：">
-            <el-date-picker :style="{width: '200px'}" @change="handleGetDataList" value-format="yyyy-MM-dd HH:mm:ss" default-time="00:00:00" placeholder="请选择日期" type="datetime" v-model="form.crteTime"></el-date-picker>
+            <el-date-picker :style="{width: '200px'}"
+                            @change="handleGetDataList"
+                            value-format="yyyy-MM-dd HH:mm:ss"
+                            default-time="00:00:00"
+                            placeholder="请选择日期"
+                            type="datetime"
+                            v-model="form.crteTime"></el-date-picker>
           </el-form-item>
         </el-form>
       </div>
       <div class="search_btns">
-        <el-button size="mini" type="default" @click="handleReset">重置</el-button>
-        <el-button size="mini" type="primary" @click="handleSearch">查询</el-button>
+        <el-button size="mini"
+                   type="default"
+                   @click="handleReset">重置</el-button>
+        <el-button size="mini"
+                   type="primary"
+                   @click="handleSearch">查询</el-button>
       </div>
     </div>
     <div class="content__container">
-      <div class="item_container add_board-box" @click="handleAddBoard">
-        <img src="../../assets/add.png" width="52" class="add-icon" />
+      <div class="item_container add_board-box"
+           @click="handleAddBoard">
+        <img src="../../assets/add.png"
+             width="52"
+             class="add-icon" />
         <p class="add-text">新增大屏</p>
       </div>
       <template v-if="boardList && boardList.length > 0">
-        <div class="item_container" v-for="item in boardList" :key="item.scrId" @click="handleBoardClick(item)">
-          <img class="bg-img" v-if="item.scrThum" :src="item.scrThum" alt="">
+        <div class="item_container"
+             v-for="item in boardList"
+             :key="item.scrId"
+             @click="handleBoardClick(item)">
+          <img class="bg-img"
+               v-if="item.scrThum"
+               :src="item.scrThum"
+               alt="">
           <div class="title-date">
-            <div class="title" v-if="item.scrName">{{item.scrName}}</div>
-            <div class="title" v-else>未命名</div>
-            <div class="date" v-if="item.updtTime ">{{item.updtTime}}</div>
-            <div class="date" v-else>--</div>
+            <div class="title"
+                 v-if="item.scrName">{{item.scrName}}</div>
+            <div class="title"
+                 v-else>未命名</div>
+            <div class="date"
+                 v-if="item.updtTime ">{{item.updtTime}}</div>
+            <div class="date"
+                 v-else>--</div>
           </div>
         </div>
       </template>
@@ -51,8 +88,8 @@ import {
   getScreenCount
 } from '@/views/EditChartView/api/index.js';
 const CHECK_PENDING = '1'; // 待审核
-const PUBLISH_PENDING = '2'; // 待发布
-const RETURN_BACK = '3'; // 退回
+const PUBLISH_PENDING = '2'; // 待提交
+const RETURN_BACK = '3'; // 驳回
 const DELETED = '4'; // 已删除
 const IN_USE = '5'; //  使用中
 export default {
@@ -105,7 +142,6 @@ export default {
     this.IN_USE = IN_USE;
   },
   methods: {
-    
     handleGetCouunt() {
       getScreenCount().then((res) => {
         if (res.code === 0 && res.type === 'success') {
@@ -155,9 +191,19 @@ export default {
         path: '/board-config-manage/add'
       });
     },
-    handleBoardClick(item){
+    handleBoardClick(item) {
+      if (item.status === CHECK_PENDING) {
+        this.$message.warning('待审核状态不可编辑');
+        return;
+      }
+
+      if (item.status === IN_USE) {
+        this.$message.warning('使用中状态不可编辑');
+        return;
+      }
+
       window.open(`/#/edit-chart-view?code=${item.scrId}`);
-    },  
+    }
   }
 };
 </script>
